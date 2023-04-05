cb47c6c2c6626e1e3225c784631390defd7d3c40
Oniya Daniel <daniel.prince84@gmail.com>
2023-04-05 15:00:05 +0100

    completed route scaffolding

--- internals/backend/server/http.go
+++ internals/backend/server/http.go
@@ -5,8 +5,48 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func RegisterBackendRoutes(*configPkg.HttpStub) *fiber.App {
+func RegisterBackendRoutes(http *configPkg.HttpStub) *fiber.App {
 	app := fiber.New()
 
+	// get list of backend connections
+	app.Get("/connections")
+
+	// test a backend connections
+	app.Post("/connections/:backend/test")
+
+	// get a list of lightning nodes connected to a backend
+	app.Get("/connections/:backend/ln")
+
+	// get the details of a lightning node
+	app.Get("/ln/:ln")
+
+	// test lightning node connection/status/availability
+	app.Post("/ln/:ln/test")
+
+	// view the details of a lightning node
+	app.Get("/ln/:ln/config")
+
+	// create lightning node configuration on a backend
+	app.Post("/ln/:ln/config")
+
+	// get list of connected peers to lightning node
+	app.Get("/ln/:ln/peers")
+
+	// connect a lightning node to a peer
+	app.Post("/ln/:ln/peers")
+
+	// get list of open lightning payment channels
+	app.Get("/ln/:ln/channels")
+
+	// open a lightning payment channel
+	app.Post("/ln/:ln/channels")
+
+	// close a lightning payment channel
+	app.Post("/ln/:ln/channels/close")
+
+	// close a lightning payment channel
+	app.Post("/ln/:ln/channels/force-close")
+
+	http.Router.Mount("/backend", app)
 	return app
 }
